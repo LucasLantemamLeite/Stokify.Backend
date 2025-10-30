1ec0b21dbb174342d4325b7b7451e93e64f5dd48
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-29 22:38:20 -0300

    feat: add global using for Enums

--- Stokify.Domain/GlobalUsing.cs
+++ Stokify.Domain/GlobalUsing.cs
@@ -1,3 +1,4 @@
 global using Stokify.Domain.Abstracts.Entity;
 global using Stokify.Domain.Abstracts.Markers.Aggregates;
 global using Stokify.Domain.Abstracts.Markers.ValueObject;
+global using Stokify.Domain.Enums;
