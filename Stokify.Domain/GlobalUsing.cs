cae9236f95ddadd6b3958d1a38d7789c30bd9459
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-31 15:42:28 -0300

    feat: fix and add new global using

--- Stokify.Domain/GlobalUsing.cs
+++ Stokify.Domain/GlobalUsing.cs
@@ -1,5 +1,5 @@
 global using Stokify.Domain.Abstracts.Entity;
-global using Stokify.Domain.Abstracts.Markers.Aggregates;
-global using Stokify.Domain.Abstracts.Markers.ValueObject;
+global using Stokify.Domain.Abstracts.Markers;
 global using Stokify.Domain.Enums;
 global using Stokify.Domain.Validations.Exceptions.Common;
+global using Stokify.Domain.ValueObjects.Common;
