9f9a0902672e7761e8950db9aabba50cc09ec958
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-31 12:58:24 -0300

    feat: add global using for Common Exceptions

--- Stokify.Domain/GlobalUsing.cs
+++ Stokify.Domain/GlobalUsing.cs
@@ -2,3 +2,4 @@ global using Stokify.Domain.Abstracts.Entity;
 global using Stokify.Domain.Abstracts.Markers.Aggregates;
 global using Stokify.Domain.Abstracts.Markers.ValueObject;
 global using Stokify.Domain.Enums;
+global using Stokify.Domain.Validations.Exceptions.Common;
