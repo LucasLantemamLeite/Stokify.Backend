c49b334541b38fa3de2ac1c6e56cca34e29c7bc6
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-31 17:17:45 -0300

    feat: add generic type to define the Id

--- Stokify.Domain/Abstracts/Entity/Entity.cs
+++ Stokify.Domain/Abstracts/Entity/Entity.cs
@@ -1,8 +1,16 @@
 namespace Stokify.Domain.Abstracts.Entity;
 
-public abstract class Entity
+public abstract class Entity<T>
 {
-    protected Guid Id { get; set; }
-    protected Entity() => Id = Guid.NewGuid();
-    protected Entity(Guid id) => Id = id;
+    public T? Id { get; protected set; }
+    protected Entity()
+    {
+        if (typeof(T) == typeof(Guid))
+            Id = (T)(object)Guid.NewGuid();
+
+        else if (typeof(T) != typeof(int))
+            throw new ArgumentException($"O tipo {typeof(T).Name} não é suportado como identificador de entidade. Use Guid para Aggregate Roots ou int para entidades auxiliares.");
+    }
+
+    protected Entity(T id) => Id = id;
 }
