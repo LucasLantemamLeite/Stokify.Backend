6ce97180b54d7bff76423dacae573357a5656682
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-31 22:56:29 -0300

    feat: add the global using import for Test Layer

---
+++ Stokify.Test/GlobalUsing.cs
@@ -0,0 +1,4 @@
+global using Stokify.Domain.ValueObjects.Common;
+global using Stokify.Domain.ValueObjects.Inventory;
+global using Stokify.Domain.ValueObjects.Product;
+global using Stokify.Domain.Validations.Exceptions.Common;
