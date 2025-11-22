bc53c5ea3f725b37f1ac5d4f8012c3f910a9ed9e
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-11-22 17:50:04 -0300

    feat: adiciona a chamada dos métodos de configuração

--- Stokify.Api/Program.cs
+++ Stokify.Api/Program.cs
@@ -1,4 +1,19 @@
+using Stokify.Api.Configuration.App;
+using Stokify.Api.Configuration.Builder;
+using Stokify.Api.Configuration.Injections;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.ApplyConfig();
+
+builder.Services
+    .AddQueries()
+    .AddRepositories()
+    .AddUseCases()
+    .AddServices(builder.Configuration);
+
 var app = builder.Build();
+
+app.ApplyConfig();
 
 app.Run();
