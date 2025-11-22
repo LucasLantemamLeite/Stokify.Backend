38889148e12e56dee0134d2abb4276e257d528d0
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-11-22 17:09:19 -0300

    refactor: altera nome da pasta para evitar conflito com namespace

--- Stokify.Application/Results/Result.cs
+++ Stokify.Application/Results/Result.cs
@@ -1,4 +1,4 @@
-namespace Stokify.Application.Abstracts.Result;
+namespace Stokify.Application.Results;
 
 public sealed class Result<TResult>(string message, bool success = true, TResult? result = default)
 {
@@ -6,6 +6,6 @@ public sealed class Result<TResult>(string message, bool success = true, TResult
     public bool IsSuccess { get; init; } = success;
     public TResult? Data { get; init; } = result;
 
-    public static Result<TResult> Ok(string message, TResult? data) => new(message, true, data);
+    public static Result<TResult> Ok(string message, TResult? data = default) => new(message, true, data);
     public static Result<TResult> Fail(string message) => new(message, false);
 }
