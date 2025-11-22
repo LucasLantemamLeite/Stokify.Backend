a6a9201752a21e82c4f65297c2985de8f311bd90
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-11-22 16:12:50 -0300

    feat: cria a classe para resultados das operações

---
+++ Stokify.Application/Abstracts/Result/Result.cs
@@ -0,0 +1,11 @@
+namespace Stokify.Application.Abstracts.Result;
+
+public sealed class Result<TResult>(string message, bool success = true, TResult? result = default)
+{
+    public string Message { get; init; } = message;
+    public bool IsSuccess { get; init; } = success;
+    public TResult? Data { get; init; } = result;
+
+    public static Result<TResult> Ok(string message, TResult? data) => new(message, true, data);
+    public static Result<TResult> Fail(string message) => new(message, false);
+}
