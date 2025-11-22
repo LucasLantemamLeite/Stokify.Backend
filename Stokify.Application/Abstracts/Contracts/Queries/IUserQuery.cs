e0b48c44e319263a8b5404c59acef18bc923b5c8
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-11-22 15:17:39 -0300

    feat: cria o contrato para manipular dados dos usuários

---
+++ Stokify.Application/Abstracts/Contracts/Queries/IUserQuery.cs
@@ -0,0 +1,16 @@
+using Stokify.Domain.Entities;
+
+namespace Stokify.Application.Abstracts.Contracts.Queries;
+
+public interface IUserQuery : IQuery<User>
+{
+    Task<IEnumerable<User>> GetByNameAsync(string name);
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByPhoneAsync(string phone);
+    Task<IEnumerable<User>> GetByAccessAsync(string phone);
+    Task<IEnumerable<User>> GetByCreatedAtAsync(DateTime created);
+    Task<IEnumerable<User>> GetByUpdatedAtAsync(DateTime updated);
+    Task<IEnumerable<User>> GetByActive(bool active);
+    Task<bool> EmailInUseAsync(string email);
+    Task<bool> PhoneInUseAsync(string phone);
+}
---
+++ Stokify.Application/Abstracts/Contracts/Repositories/IUserRepository.cs
@@ -0,0 +1,5 @@
+using Stokify.Domain.Entities;
+
+namespace Stokify.Application.Abstracts.Contracts.Repositories;
+
+public interface IUserRepository : IRepository<User> { }
