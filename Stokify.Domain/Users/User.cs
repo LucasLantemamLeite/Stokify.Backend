15dba0751bcc1d530ac5c5465ce4f812cfc3fea6
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-11-27 16:20:20 -0300

    feat: cria a entidade raiz de usuário

---
+++ Stokify.Domain/Users/User.cs
@@ -0,0 +1,35 @@
+using System.Collections.ObjectModel;
+using Flunt.Notifications;
+using Stokify.Domain.Abstractions.Base;
+using Stokify.Domain.Abstractions.Contracts;
+using Stokify.Domain.Abstractions.Markers;
+using Stokify.Domain.Shared.ValueObjects;
+using Stokify.Domain.Users.ValueObjects;
+
+namespace Stokify.Domain.Users;
+
+public sealed class User : Entity, IAggregateRoot, IValidatable
+{
+    public Name Name { get; private set; } = null!;
+    public Email Email { get; private set; } = null!;
+    public Phone Phone { get; private set; } = null!;
+    public Access Access { get; private set; } = new();
+    public CreatedAt CreatedAt { get; private set; } = new();
+    public UpdatedAt UpdatedAt { get; private set; } = new();
+    public Active Active { get; private set; } = new();
+
+    // Validate
+    private readonly List<Notification> _notification = [];
+    public ReadOnlyCollection<Notification> Notification => _notification.AsReadOnly();
+    public bool Validate()
+    {
+        _notification.Clear();
+
+        _notification.AddRange(Name.Notifications);
+        _notification.AddRange(Email.Notifications);
+        _notification.AddRange(Phone.Notifications);
+        _notification.AddRange(Access.Notifications);
+
+        return _notification.Count == 0;
+    }
+}
