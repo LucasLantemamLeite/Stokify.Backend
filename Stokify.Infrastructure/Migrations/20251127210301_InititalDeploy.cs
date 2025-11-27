0a4c04aa0cae9677cf0e30389635b0115345d128
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-11-27 18:05:41 -0300

    chore: arquivos de migrações do banco de dados

---
+++ Stokify.Infrastructure/Migrations/20251127210301_InititalDeploy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stokify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InititalDeploy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Phone = table.Column<string>(type: "varchar(15)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Access = table.Column<short>(type: "smallint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "Unique_Key_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "Unique_Key_Users_Phone",
+                table: "Users",
+                column: "Phone",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
