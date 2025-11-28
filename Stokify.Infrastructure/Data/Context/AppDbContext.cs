e4b2730cdd5da2ec31a846950627c9bd1c40b082
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-11-28 16:42:35 -0300

    feat: adiciona suporte para injeção de dependência do AppDbContext

--- Stokify.Infrastructure/Data/Context/AppDbContext.cs
+++ Stokify.Infrastructure/Data/Context/AppDbContext.cs
@@ -3,12 +3,9 @@ using Stokify.Domain.Users;
 
 namespace Stokify.Infrastructure.Data.Context;
 
-public sealed class AppDbContext : DbContext
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql("Host=localhost; Port=5432;Database=StokifyDb;Username=postgres;Password=postgres");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
