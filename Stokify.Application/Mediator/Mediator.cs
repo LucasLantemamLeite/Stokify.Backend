983d8fa2cef279fcf9a4fa427e4b46f02e4537ea
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-11-27 21:23:44 -0300

    feat: implementa via reflection o contrato do IMediator

---
+++ Stokify.Application/Mediator/Mediator.cs
@@ -0,0 +1,22 @@
+using Stokify.Application.Abstractions.Mediator;
+
+namespace Stokify.Application.Mediator;
+
+public sealed class Mediator : IMediator
+{
+    private readonly IServiceProvider _provider;
+
+    public Mediator(IServiceProvider provider)
+        => _provider = provider;
+
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken token)
+    {
+        var requestType = request.GetType();
+
+        var handlerType = typeof(IHandler<,>).MakeGenericType(requestType, typeof(TResponse));
+
+        dynamic handler = _provider.GetService(handlerType) ?? throw new Exception($"Handler not found for {requestType.Name}");
+
+        return handler.Handle((dynamic)request);
+    }
+}
